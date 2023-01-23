f43fb454bb9d0978fb3b9b786672b989d439925a
ryuken73 <ryuken73@gmail.com>
2023-01-24 00:43:05 +0900

    updating: enumerating capture devices

--- src/SelectMediaDevices.js
+++ src/SelectMediaDevices.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
-export default function SelectMediaDevices() {
-    const getMediaDevices = React.useCallback(() => {
+async function openCamera(cameraId, minWidth, minHeight) {
+    const constraints = {
+        'audio': {'echoCancellation': true},
+        'video': {
+            'deviceId': cameraId,
+            'width': {'min': minWidth},
+            'height': {'min': minHeight}
+            }
+        }
 
+    return await navigator.mediaDevices.getUserMedia(constraints);
+}
+
+export default function SelectMediaDevices() {
+    const [devices, setDevices] = React.useState([]);
+    const getMediaDevices = React.useCallback(() => {
+        async function getConnectedDevices(type) {
+            const devices = await navigator.mediaDevices.enumerateDevices();
+            console.log(devices)
+            return devices.filter(device => device.kind === type)
+        }
+        getConnectedDevices('videoinput')
+        .then(devices => {
+            setDevices(devices);
+        });
     }, [])
     return (
-        <button onClick={getMediaDevices}>refresh media deivces</button>
-  )
+        <>
+            <button onClick={getMediaDevices}>refresh media deivces</button>
+            {devices.map(device => (
+                <div key={device.deviceId}>{device.label}</div>
+            ))}
+        </>
+    )
 }
